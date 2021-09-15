4041f08ce8841325548e70173877e11af5a378ae
Dan Gilleland <dgilleland@nait.ca>
2021-09-15 09:41:06 -0600

    Lesson 005 - Started English/Klingon Dictionary

---
+++ src/003/Sample Quiz/CSharp.Language.Quiz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using CSharp.Language.Quiz.Entities;
+
+namespace CSharp.Language.Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/005/Travel Aid/WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Data;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        // This property will receive the value(s) from the form when a POST request is generated
+        [BindProperty]
+        public Translation PopularPhrase { get; set; }
+
+        public void OnGet()
+        {
+            // This runs when the request method is a GET
+        }
+
+        public void OnPost()
+        {
+            // This runs when the request method is a POST
+        }
+    }
+}
