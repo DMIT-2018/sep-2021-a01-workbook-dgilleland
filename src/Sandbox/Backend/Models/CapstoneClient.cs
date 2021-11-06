72fe2412db6a22c3577d3c98abf0328c35fa2b31
Dan Gilleland <dgilleland@nait.ca>
2021-11-05 20:29:52 -0600

    AdHoc demo in the SandBox.

---
+++ src/Sandbox/Backend/Models/CapstoneClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class CapstoneClient
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string Slogan { get; set; }
+        public string ContactName { get; set; }
+        public bool Confirmed { get; set; }
+    }
+}
